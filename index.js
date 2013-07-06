adfa668b014e75849eeb00089b7b42abb0c9b26d
Mikola Lysenko <mikolalysenko@gmail.com>
2013-07-06 01:13:37 -0500

    upgrading to game-shell 0.1.0

--- index.js
+++ index.js
@@ -18,10 +18,8 @@ function createGLShell(options) {
     shell.element.appendChild(canvas)
 
     //Load width/height
-    var width = shell.element.clientWidth
-    var height = shell.element.clientHeight    
-    canvas.width = width
-    canvas.height = height
+    canvas.width = shell.width
+    canvas.height = shell.height
     
     //Try initializing WebGL
     var gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl")
@@ -32,12 +30,6 @@ function createGLShell(options) {
     //Add variables to game-shell
     shell.canvas = canvas
     shell.gl = gl
-    Object.defineProperty(shell, "width", {
-      get: function() { return width }
-    })
-    Object.defineProperty(shell, "height", {
-      get: function() { return height }
-    })
     
     //Load default parameters
     shell.clearFlags = options.clearFlags === undefined ? (gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT) : options.clearFlags
@@ -45,37 +37,19 @@ function createGLShell(options) {
     shell.clearDepth = options.clearDepth || 1.0
     shell.clearStencil = options.clearStencil || 0
     
-    //Called when the window is resized or DOM element updates
-    function handleResize() {
-      var w = shell.element.clientWidth|0
-      var h = shell.element.clientHeight|0
-      if((w !== width) || (h !== height)) {
-        width = w
-        height = h
-        canvas.width = width
-        canvas.height = height
-        shell.emit("resize", width, height)
-      }
-    }
-    
-    if(typeof MutationObserver !== "undefined") {
-      var observer = new MutationObserver(handleResize)
-      observer.observe(shell.element, {
-        attributes: true,
-        subtree: true
-      })
-    } else {
-      shell.element.addEventListener("DOMSubtreeModified", handleResize)
-    }
-    window.addEventListener("resize", handleResize)
+    shell.on("resize", function(w, h) {
+      canvas.width = w
+      canvas.height = h
+    })
 
+    //Hook render event
     shell.on("render", function renderGLNow(t) {
     
       //Bind default framebuffer
       gl.bindFramebuffer(gl.FRAMEBUFFER, null)
       
       //Set viewport
-      gl.viewport(0, 0, width, height)
+      gl.viewport(0, 0, shell.width, shell.height)
       
       //Clear buffers
       if(shell.clearFlags & gl.STENCIL_BUFFER_BIT) {
